38980203fea8c9d7ba5919dff3c5367139b2a51e
nswon <swnam0729@gmail.com>
2023-11-09 12:24:49 +0900

    setting

---
+++ src/test/java/com/budgetmanagement/budgetmanagement/BudgetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetmanagement.budgetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
