0d5a4fb1bc55341886bad6de9b2b7085256a4267
nswon <swnam0729@gmail.com>
2023-12-26 22:18:50 +0900

    fix: budget 추천 dto 변환 로직 수정

--- src/main/java/com/budgetmanagement/budgetmanagement/controller/budget/response/BudgetsRecommendResponse.java
+++ src/main/java/com/budgetmanagement/budgetmanagement/controller/budget/response/BudgetsRecommendResponse.java
@@ -4,11 +4,13 @@ import com.budgetmanagement.budgetmanagement.domain.budget.BudgetContent;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
 public record BudgetsRecommendResponse(List<BudgetRecommendResponse> response) {
     public static BudgetsRecommendResponse of(List<BudgetContent> budgets) {
-        List<BudgetRecommendResponse> response = budgets.stream()
+        return budgets.stream()
                 .map(each -> new BudgetRecommendResponse(each.category(), each.amount()))
-                .toList();
-        return new BudgetsRecommendResponse(response);
+                .collect(collectingAndThen(toList(), BudgetsRecommendResponse::new));
     }
 }
